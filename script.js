bc8abe85d07a4fe6864df89a7299c84650c1c27a
sovonbd <sovonsdp@gmail.com>
2023-08-16 00:34:01 -0500

    updated

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ function textStyle(elementID, changeType, changeTo) {
 document.getElementById("font-size").addEventListener("change", function () {
   document.getElementById("textarea").style.fontSize = document.getElementById("font-size").value + "px";
 });
-
+document.getElementById("case").addEventListener("change", function () {
+  document.getElementById("textarea").style.textTransform = document.getElementById("case").value;
+});
 document.getElementById("color").addEventListener("input", function (event) {
   const text = document.getElementById("textarea");
   if (text) {
@@ -21,5 +23,5 @@ textStyle("center", "textAlign", "center");
 textStyle("right", "textAlign", "right");
 textStyle("justify", "textAlign", "justify");
 textStyle("upper", "textTransform", "uppercase");
-textStyle("lower", "textTransform", "lowercase");
-textStyle("color", "color", "lowercase");
+// textStyle("lower", "textTransform", "lowercase");
+// textStyle("color", "color", "lowercase");
